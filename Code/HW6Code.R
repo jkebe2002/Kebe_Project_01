096ca8b012d73f0b0e9e09d496e0113fdb82d34a
jkebe2002 <jacob.kebe@cuanschutz.edu>
2024-10-03 15:48:09 -0600

    HW 02 report - update to remove 999 outlier

--- Code/HW6Code.R
+++ Code/HW6Code.R
@@ -12,7 +12,9 @@ avg_iq_far <- mean(leadData[leadData$Smelter == "Far",]$IQ)
 boxplot(IQ~Smelter, data = leadData)
 
 
-##Other methods for data visualization that were tried and uneffective/unasthetic
+
+
+##Other methods for data visualization that were tried and ineffective/unaesthetic
 p <- ggplot(leadData, aes(x=Smelter, y=IQ)) + 
   geom_dotplot(binaxis='y', stackdir='center')
 
@@ -40,4 +42,25 @@ p<- ggplot(leadData, aes(x=Smelter, y=IQ) +
            
            smelter_bin <- as.numeric(leadData$Smelter == "Near")
            leadData <- c(leadData, smelter_bin)
+           
+           
+           
+           
+           
+           #Creating a table using kable:
+           library(knitr)
+           #dfSummary(leadData)
+           near_dat <- summary(leadData[leadData$Smelter == "Near",])
+           kable(near_dat, caption = "IQ Summary Stats, Near Smelter")
+           far_dat <- summary(leadData[leadData$Smelter == "Far",])
+           kable(far_dat, caption = "IQ Summary Stats, Far from Smelter")
+           
+           
+           #Method that worked
+           near_iq_stats <- summary(leadData[leadData$Smelter=="Near",]$IQ)
+           far_iq_stats <- summary(leadData[leadData$Smelter=="Far",]$IQ)
+           stat_names <- c("Min.","1st Qu.", "Median","Mean","3rd Qu.", "Max")
+           iq_stats <- data.frame(nearlab =stat_names, Near = as.numeric(near_iq_stats), farlab = stat_names, Far = as.numeric(far_iq_stats))
+           
+           kable(iq_stats, col.names= c("","IQ: Near Group", "", "IQ: Far Group"), digits = 2)
            
